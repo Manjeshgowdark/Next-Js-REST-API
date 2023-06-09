fe98daf48bd0b3c0f4a186f6402a8a09dc504f5d
Manjesh Gowda R K <manjeshgowdark@gmail.com>
2023-06-09 10:07:27 +0530

    first commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server"
+import { limiter } from "../config/limiter"
+
+export async function GET(request: Request) {
+
+  const remaining = await limiter.removeTokens(1)
+  console.log('remaining: ',remaining)
+  
+  const origin = request.headers.get('origin')
+  
+
+  if(remaining < 0){
+    return new NextResponse(null, {
+      status: 429,
+      statusText: "Too Many Requests",
+      headers:{
+        'Access-control-Allow-Origin': origin || '*',
+        'Content-Type' : 'text/plain',
+      }
+    })
+  }
+
+  return new Response('Hello, Next.js!')
+}
